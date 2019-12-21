02eccaace513debf852c1050b522deb1962350ff
Caleb Vincent <vincent_806@yahoo.com>
2019-12-20 18:14:53 -0600

    Created skeleton html/css/js

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// create a loop that displays all the slots from 9am - 5pm
+var timeblocks= $(".container")
+
+function showQuestions(){
+   $("<div></div>").addClass("col-md-1");
+    $("<div></div>").addClass("col-md-10");
+   $("<div></div>").addClass("col-md-1");
+
+   for (var i = 0; i < 9; i++);
+   
+}
+
+showQuestions.append(timeblocks)
