ab5820b894d6a840f08f3078b37e83dad8c83650
CalebVincent <vincent_806@yahoo.com>
2020-02-02 13:39:17 -0600

    Began new javascript w/ some psudo-code, revised HTML

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,50 +1,17 @@
-// display time date + date in jumbotron
-$(document).ready(function () {
-// store html reference in variable
-var currentDayEl = $("#currentDay");
-var inputBlockEl = $(".time-block");
-var saveBtns = $(".saveBtn");
+$(document).ready(function() {
+    // function to set the time (in jumbotron)
+    // make a variable to hit the html for current time element
+    // .text (moment)
 
-// create function to add time 
-var update = function () {
-    currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    // update time slots
 
-    var now = moment().hour();
-    
-    inputBlockEl.each(function (i, element) {
-        element = $(element);
-        if (now > element.attr("data-hour")) {
-            element.addClass("past").removeClass("future present");
-        } else if (now == element.attr("data-hour")) {
-            element.addClass("present").removeClass("future past");
-        }
-    });
-}
-
-setInterval(update, 1000);
-$('.toast').toast(500);
-
-
-saveBtns.on("click", function (event) {
-    $(".toast").toast('show');
-        var button = $(this);
-        var btnData = $(this).attr("data-time");
-        var descriptionTarget = $("#description" + btnData);
-        localStorage.setItem(descriptionTarget.attr("id"), descriptionTarget.val());
-    });
-
-    $("#description9").text(localStorage.getItem("description9"));
-    $("#description10").text(localStorage.getItem("description10"));
-    $("#description11").text(localStorage.getItem("description11"));
-    $("#description12").text(localStorage.getItem("description12"));
-    $("#description13").text(localStorage.getItem("description13"));
-    $("#description14").text(localStorage.getItem("description14"));
-    $("#description15").text(localStorage.getItem("description15"));
-    $("#description16").text(localStorage.getItem("description16"));
-    $("#description17").text(localStorage.getItem("description17"));
-
-    $("#clearBtn").on("click", function () {
-        localStorage.clear();
-        $(".description").text("");
-    });
 })
+
+
+// function to save to local storage
+    // on click localStorage.setItem
+
+// function to load local storage
+    // target html element then .text(localStorage.getItem)
+
+// function to clear local storage
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Link jQuery, Moment, Bootstrap
+
 An application to help manage time during business hours that changes colors with the current time.
 
 ![daily calender from 9am-5pm](https://raw.githubusercontent.com/CalebVincent/DayPlannerHW/master/Assets/deployedsite.png)
